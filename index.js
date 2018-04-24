91e8c61162771cbb15f6d561fbc5efd2f4269401
Jose Aguayo <jpaguayo11@yahoo.es>
2018-04-24 20:14:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     console.log("I can't hear you!")
+  } else if (string.toUpperCase() === string) {
+    console.log("YES INDEED!")
+  } else (string === "I love you, Grandma.") {
+    console.log("I love you, too.")
   }
 }
 sayHiToGrandma("hey")
